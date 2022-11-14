b35df39232dcdc933f9e562764aa7e0bb990b4d7
storm <sahalisa17@gmail.com>
2022-11-14 18:10:15 +0300

    Перенос на scss

--- src/components/album/AlbumCard.vue
+++ src/components/album/AlbumCard.vue
@@ -10,26 +10,27 @@
             :src="album.ogImage"
             :width="200"
             type="album"
+            class="album-card__image"
         />
 
-        <h3 class="title">
+        <h3 class="album-card__title">
             {{ album.title }}
         </h3>
         <ArtistsLinks
             v-if="album.artists && album.type !== 'compilation'"
             :artists="album.artists"
-            class="author"
+            class="album-card__author"
         />
-        <div class="album-card-footer">
+        <div class="album-card__footer">
             <p
                 v-if="album.type"
-                class="type"
+                class="album-card__type"
             >
                 {{ type }}
             </p>
             <p
                 v-if="album.year"
-                class="year"
+                class="album-card__year"
             >
                 {{ album.year }}
             </p>
@@ -75,7 +76,7 @@ const type = computed(() => {
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .album-card {
     background: #292C3B;
     border-radius: 6px;
@@ -86,71 +87,73 @@ const type = computed(() => {
     width: 100%;
     height: 235px;
     max-width: 160px;
-}
 
-.album-card img {
-    margin-bottom: 12px;
-}
+    &__image {
+        margin-bottom: 12px;
 
-.title {
-    font-size: 13px;
-    font-weight: 500;
-    -webkit-line-clamp: 2;
-    display: -webkit-box;
-    line-height: 16px;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    text-overflow: ellipsis;
-}
+        button {
+            opacity: 0;
+            transition: 0.2s;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            background: var(--main-color);
+            width: 45px;
+            height: 45px;
+            border-radius: 999px;
+            z-index: 1000;
+        }
+    }
 
-.author {
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 16px;
-    color: #8E929C;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    white-space: nowrap;
-    align-items: center;
-}
+    &__title {
+        font-size: 13px;
+        font-weight: 500;
+        -webkit-line-clamp: 2;
+        display: -webkit-box;
+        line-height: 16px;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
 
-.type {
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 16px;
-    color: #8E929C;
-}
+    &__author {
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 16px;
+        color: #8E929C;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+        align-items: center;
+    }
 
-.year {
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 16px;
-    color: #8E929C;
-    margin-left: auto;
-}
+    &__type {
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 16px;
+        color: #8E929C;
+    }
 
-.album-card-footer {
-    display: flex;
-    flex-direction: row;
-    margin-top: auto;
-}
+    &__year {
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 16px;
+        color: #8E929C;
+        margin-left: auto;
+    }
 
-.album-card-image button {
-    opacity: 0;
-    transition: 0.2s;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    background: var(--main-color);
-    width: 45px;
-    height: 45px;
-    border-radius: 999px;
-    z-index: 1000;
-}
+    &__footer {
+        display: flex;
+        flex-direction: row;
+        margin-top: auto;
+    }
 
-.album-card:hover .album-card-image button {
-    opacity: 1;
-    transition: 0.2s;
+    &:hover {
+        &__image button {
+            opacity: 1;
+            transition: 0.2s;
+        }
+    }
 }
 </style>
